f6bc08fab0fdcec0de599d76bd6e17be73b16ccb
Mz_1326 <1546860452@qq.com>
2017-08-21 21:26:18 +0800

    package releases

--- drop.js
+++ drop.js
@@ -1,16 +1,22 @@
-function drag(id){
+
+	function drag(id){
 	var obj = document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
-	obj.onmousedown = function(ev){
+	obj.onmousedown=function(ev){
 		var oEvent = ev ||window.event;
 		disX = oEvent.pageX - obj.offsetLeft;
 		disY = oEvent.pageY - obj.offsetTop;
 
-		obj.onmousemove = function(ev){
+		document.onmousemove = function(ev){
 			obj.style.left = ev.pageX -disX +"px";
 			obj.style.top = ev.pageY -disY +"px";
 		}
-		obj.onmouseup = function(){}
+		document.onmouseup = function(){
+			document.onmousemove =null;
+			document.onmouseup = null;
+		}
 	}
+	return false;
 }
+
